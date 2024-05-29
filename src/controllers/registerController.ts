1a7ccc89f5a85bb308c23f6197aff4ba29ed3cfe
theEast17 <nagarpoorv2001@gmail.com>
2024-05-29 13:58:29 +0530

    add error handling

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "../models/userModel";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 
@@ -12,54 +12,56 @@ const registerController = async (
 ) => {
   const { name, email, password } = req.body;
 
-//   check validation
+  //   check validation
   if (!name || !email || !password) {
-    const error = createHttpError(400, "All fields are required");   
+    const error = createHttpError(400, "All fields are required");
     return next(error);
   }
 
-//   check whether the user already exists or not
+  //   check whether the user already exists or not
 
-  const userExists=await userModel.findOne({email})  // it returns null or document
+  try {
+    const userExists = await userModel.findOne({ email }); // it returns null or document
 
-  if(userExists){
-    const error = createHttpError(400, "User already exists with this email");   
-    return next(error);
+    if (userExists) {
+      const error = createHttpError(400, "User already exists with this email");
+      return next(error);
+    }
+
+    //   if user not exixsts then we need to store into the database
+    // we need to store passwords in hash bcrypt form
+
+    const hashPassword = await bcrypt.hash(password, 10); // there are 2 parameters in hash 1st name of property and 2nd is saltOnRounds  more detail on line No. 269
+
+    //  now we are ready to store into the database
+
+    const newUser = await userModel.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    // now one more thing when we are creating user we need to attach one more thing called jwt json web token for security purpose
+
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    res.json({ accessToken: token });
+
+    // if (newUser) {
+    //     res.status(201).json({
+    //         _id: newUser._id,
+    //         name: newUser.name,
+    //         email: newUser.email,
+    //     })
+    // } else {
+    //     const error = createHttpError(400, "Invalid Credentials");
+    //     return next(error);
+    // }
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting user"));
   }
-
-
-//   if user not exixsts then we need to store into the database
-// we need to store passwords in hash bcrypt form
-
-const hashPassword= await bcrypt.hash(password,10)   // there are 2 parameters in hash 1st name of property and 2nd is saltOnRounds  more detail on line No. 269 
-
-//  now we are ready to store into the database
-
-const newUser=await userModel.create({
-    name,
-    email,
-    password:hashPassword
-})
-
-
-// now one more thing when we are creating user we need to attach one more thing called jwt json web token for security purpose
-
-
-const token=sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:'7d'})
-
-res.json({accessToken:token})
-
-// if (newUser) {
-//     res.status(201).json({
-//         _id: newUser._id,
-//         name: newUser.name,
-//         email: newUser.email,
-//     })
-// } else {
-//     const error = createHttpError(400, "Invalid Credentials");   
-//     return next(error);
-// }
-
 };
 
 export default registerController;
