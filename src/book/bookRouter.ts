8e41a895a7f7fa5f4df7b0f50be9b25a0c53c6d9
theEast17 <nagarpoorv2001@gmail.com>
2024-05-30 18:07:12 +0530

    update book

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -3,12 +3,13 @@ import createBook from "../controllers/createBook";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
+import updateBook from "../controllers/updateBook";
 
 const bookRouter = express.Router();
 
 const upload = multer({
   dest: path.resolve(__dirname, "../../public/data/uploads"),
-  limits: { fileSize: 1e7 }, // 30mb
+  limits: { fileSize: 1e7 }, // 10mb
 });
 
 bookRouter.post(
@@ -19,6 +20,16 @@ bookRouter.post(
     { name: "file", maxCount: 1 },
   ]),
   createBook
+);
+
+bookRouter.put(
+  "/update/:bookId",
+  authenticate,
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  updateBook
 );
 
 export default bookRouter;
